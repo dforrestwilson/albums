7f2b908bfbe2c6645f41ba1873eb5abbf64cd241
Forrest Wilson <dforrestwilson@hotmail.com>
2017-01-26 13:28:55 -0600

    Update lendingclub.py

--- lendingclub.py
+++ lendingclub.py
@@ -1,4 +1,4 @@
-#Lending Club machine learning script
+#Lending Club machine learning script, analyze loans to predict whether or not loans will default
 import os, sys, math
 import pandas as pd
 os.chdir('*****\LendingClub')
